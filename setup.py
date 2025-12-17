851a99c8c00b053b36860576f8372664effe8436
Pt <pt.main.acc@gmail.com>
2025-12-18 02:18:51 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, Extension, find_packages
 
 __bx_version__ = '2.0'
+__framework_version__ = '0.9.0'
 
 match __bx_version__:
     case '1.0':
@@ -24,15 +25,11 @@ match __bx_version__:
         c_mod = [basemem]
 
 
-with open('README.md', 'r') as rf:
-    readme = f.read()
-
-
 setup(
     name='unitverge',
     author='Pt',
     author_email='kvantorium73.int@gmail.com',
-    version='0.3.1',
+    version=__framework_version__,
     ext_modules=c_mod,
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
@@ -54,12 +51,7 @@ setup(
     ],
     description='A metaprogramming framework for code generation and DSL creation',
     url='https://github.com/pt-main/unitverge',
+    ext_package='UnitVerge.Bytex.bx2',
     include_package_data=True,
-    long_description = readme,
-    options={
-        'bdist_wheel': {
-            'python_tag': 'cp314',
-            'plat_name': 'manylinux2014_x86_64',
-        }
-    }
+    zip_safe=False,
 )
